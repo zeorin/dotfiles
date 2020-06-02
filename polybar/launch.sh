abb4a3ec40284de4061765ef251bc75ac2af73ac
Xandor Schiefer <me@xandor.co.za>
2020-06-02 22:31:19 +0200

    fix: polybar config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
 # Wait until the processes have been shut down
 while pgrep -u "$(id -u)" -x polybar >/dev/null; do sleep 1; done
