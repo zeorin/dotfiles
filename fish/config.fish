8ab88e26d9e5b3637e6f5567c10f101f08e3d1a0
Xandor Schiefer <me@xandor.co.za>
2020-12-30 23:07:56 +0200

    feat: fish settings

---
+++ fish/config.fish
@@ -0,0 +1,37 @@
+if status is-interactive
+
+	# Tmux session chooser {{{
+		if test -e ~/.tmux/session_chooser.fish
+			source ~/.tmux/session_chooser.fish
+		end
+	# }}}
+
+	set EDITOR nvim
+
+	eval (direnv hook fish)
+
+	# Use Vi keys, and Emacs keys
+	function fish_user_key_bindings
+		# Execute this once per mode that emacs bindings should be used in
+		fish_default_key_bindings -M insert
+		# Then execute the vi-bindings so they take precedence when there's a conflict.
+		# Without --no-erase fish_vi_key_bindings will default to
+		# resetting all bindings.
+		# The argument specifies the initial mode (insert, "default" or visual).
+		fish_vi_key_bindings --no-erase insert
+	end
+
+	# Emulates vim's cursor shape behavior
+	# Set the normal and visual mode cursors to a block
+	set fish_cursor_default block
+	# Set the insert mode cursor to a line
+	set fish_cursor_insert line
+	# Set the replace mode cursor to an underscore
+	set fish_cursor_replace_one underscore
+	# The following variable can be used to configure cursor shape in
+	# visual mode, but due to fish_cursor_default, is redundant here
+	set fish_cursor_visual block
+
+	starship init fish | source
+
+end
