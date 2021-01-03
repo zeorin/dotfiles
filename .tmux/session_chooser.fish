036300c06553f161e9a69c5e1fd8d85c3aae4548
Xandor Schiefer <me@xandor.co.za>
2021-01-03 13:55:11 +0200

    fix: add missing fish session chooser script

---
+++ .tmux/session_chooser.fish
@@ -0,0 +1,25 @@
+#!/usr/bin/env fish
+if command -v tmux >/dev/null 2>&1 && [ -z "$TMUX" ]
+
+	# If this is a remote tty, allow the MOTD, banner, etc. to be seen first
+	set parent_process (ps -o comm= -p (ps -o ppid= -p $fish_pid | string trim))
+	if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ] || string match -q -- sshd $parent_process
+		echo -ne '\e[7mPress any key to continue…\e[0m';
+		set saved_tty (stty -g </dev/tty)
+		stty raw -echo
+		dd if=/dev/tty bs=1 count=1 >/dev/null 2>&1
+		stty "$saved_tty"
+	end
+
+	set tmux_unattached_sessions (tmux list-sessions -F '#{session_name} #{session_attached}' 2>/dev/null | grep ' 0$' | sed -e 's/ 0$//')
+
+	if [ -z "$tmux_unattached_sessions" ]
+		exec tmux new-session
+	else
+		set tmux_new_session (tmux new-session -dPF '#{session_name}')
+		exec tmux \
+			attach -t "$tmux_new_session" \; \
+			choose-tree -s -f '#{?session_attached,0,1}' \
+				"switch-client -t '%%'; kill-session -t '$tmux_new_session'"
+	end
+end
