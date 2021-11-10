6ab5b0044c913814c2de3847b40c4a8e7524a207
Xandor Schiefer <me@xandor.co.za>
2021-11-10 21:49:29 +0200

    feat: merge org and org-roam

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -1594,8 +1594,9 @@ in {
 
         ;; If you use `org' and don't want your org files in the default location below,
         ;; change `org-directory'. It must be set before org loads!
-        (setq org-directory "~/Documents/org")
-        (setq org-roam-directory "~/Documents/org-roam")
+        (setq org-directory "~/Documents/notes")
+        (setq org-roam-directory "~/Documents/notes")
+        (setq org-agenda-files '("~/Documents/notes" "~/Documents/notes/daily"))
 
         ;; This determines the style of line numbers in effect. If set to `nil', line
         ;; numbers are disabled. For relative line numbers, set this to `relative'.
