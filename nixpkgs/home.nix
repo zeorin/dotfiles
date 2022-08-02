d8dfc12a4741478310685f00f85f446b9bfe8156
Xandor Schiefer <me@xandor.co.za>
2022-08-02 15:40:31 +0200

    fix: disable DOOM dart module

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2525,7 +2525,7 @@ in {
                  ;;crystal           ; ruby at the speed of c
                  ;;csharp            ; unity, .NET, and mono shenanigans
                  ;;data              ; config/data formats
-                 (dart +flutter)   ; paint ui and not much else
+                 ;;(dart +flutter)   ; paint ui and not much else
                  ;;elixir            ; erlang done right
                  ;;elm               ; care for a cup of TEA?
                  emacs-lisp        ; drown in parentheses
