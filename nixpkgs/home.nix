6341248c0d1b99697a01600a715bc776fc1e3a54
Xandor Schiefer <me@xandor.co.za>
2022-08-04 09:23:29 +0200

    fix: add freedesktop.org standard dark/light mode setting

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2992,11 +2992,13 @@ in {
         pkgs.writeShellScript "set-dark-theme.sh" ''
           ${pkgs.xfce.xfconf}/bin/xfconf-query --create --type string -c xsettings -p /Net/ThemeName -s "Nordic"
           ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-dark'"
+          ${pkgs.dconf}/bin/dconf write /org/freedesktop/appearance/color-scheme "'prefer-dark'"
         '';
       "light-mode.d/gtk-theme.sh".source =
         pkgs.writeShellScript "set-light-theme.sh" ''
           ${pkgs.xfce.xfconf}/bin/xfconf-query --create --type string -c xsettings -p /Net/ThemeName -s "Nordic-Polar"
           ${pkgs.dconf}/bin/dconf write /org/gnome/desktop/interface/color-scheme "'prefer-light'"
+          ${pkgs.dconf}/bin/dconf write /org/freedesktop/appearance/color-scheme "'prefer-light'"
         '';
     };
     desktopEntries = {
