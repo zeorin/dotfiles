21160fc249aa086e75545084b6e9b07d66a4dbcf
Xandor Schiefer <me@xandor.co.za>
2022-08-16 21:17:39 +0200

    fix: correct org paths

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2419,9 +2419,9 @@ in {
 
         ;; If you use `org' and don't want your org files in the default location below,
         ;; change `org-directory'. It must be set before org loads!
-        (setq org-directory "${userDirs.documents}/notes"
-              org-roam-directory "${userDirs.documents}/notes"
-              org-agenda-files '("${userDirs.documents}/notes" "${userDirs.documents}/notes/daily"))
+        (setq org-directory "~/Documents/notes"
+              org-roam-directory "~/Documents/notes"
+              org-agenda-files '("~/Documents/notes" "~/Documents/notes/daily"))
 
         ;; This determines the style of line numbers in effect. If set to `nil', line
         ;; numbers are disabled. For relative line numbers, set this to `relative'.
