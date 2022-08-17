d859d92901961211de26b9c1ffd0880bd897bda6
Xandor Schiefer <me@xandor.co.za>
2022-08-17 17:17:28 +0200

    feat: transparent terminal background

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -252,7 +252,7 @@ let
       unsigned int tabspaces = 8;
 
       /* bg opacity */
-      float alpha = 1;
+      float alpha = 0.9;
 
       /* Terminal colors (16 first used in escape sequence) */
       static const char *colorname[] = {
