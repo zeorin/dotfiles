986e327d1db0059435744621e52d3ed2b4d60af3
Xandor Schiefer <me@xandor.co.za>
2022-09-20 20:29:32 +0200

    fix: disable emojify-mode

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2089,8 +2089,7 @@ in {
                  doom              ; what makes DOOM look the way it does
                  doom-dashboard    ; a nifty splash screen for Emacs
                  doom-quit         ; DOOM quit-message prompts when you quit Emacs
-                 (emoji +unicode)  ; 🙂
-                 fill-column       ; a `fill-column' indicator
+                 ;;(emoji +unicode)  ; 🙂
                  hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
                  ;;hydra
                  indent-guides     ; highlighted indent columns
