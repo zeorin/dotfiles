0c539ccdb9f5731a1f325c1fa2ace0081f5368cd
Xandor Schiefer <me@xandor.co.za>
2022-09-21 00:11:39 +0200

    feat: harden GPG, and enable ssh in gpg-agent

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -742,7 +742,69 @@ in {
     gpg = {
       enable = true;
       homedir = "${config.xdg.dataHome}/gnupg";
-      settings = { keyserver = "hkps://keys.openpgp.org/"; };
+      settings = {
+        # https://github.com/drduh/config/blob/master/gpg.conf
+        # https://www.gnupg.org/documentation/manuals/gnupg/GPG-Configuration-Options.html
+        # https://www.gnupg.org/documentation/manuals/gnupg/GPG-Esoteric-Options.html
+        # Use AES256, 192, or 128 as cipher
+        personal-cipher-preferences = "AES256 AES192 AES";
+        # Use SHA512, 384, or 256 as digest
+        personal-digest-preferences = "SHA512 SHA384 SHA256";
+        # Use ZLIB, BZIP2, ZIP, or no compression
+        personal-compress-preferences = "ZLIB BZIP2 ZIP Uncompressed";
+        # Default preferences for new keys
+        default-preference-list =
+          "SHA512 SHA384 SHA256 AES256 AES192 AES ZLIB BZIP2 ZIP Uncompressed";
+        # SHA512 as digest to sign keys
+        cert-digest-algo = "SHA512";
+        # SHA512 as digest for symmetric ops
+        s2k-digest-algo = "SHA512";
+        # AES256 as cipher for symmetric ops
+        s2k-cipher-algo = "AES256";
+        # UTF-8 support for compatibility
+        charset = "utf-8";
+        # Show Unix timestamps
+        fixed-list-mode = true;
+        # No comments in signature
+        no-comments = true;
+        # No version in output
+        no-emit-version = true;
+        # Disable banner
+        no-greeting = true;
+        # Long hexidecimal key format
+        keyid-format = "0xlong";
+        # Display UID validity
+        list-options = "show-uid-validity";
+        verify-options = "show-uid-validity";
+        # Display all keys and their fingerprints
+        with-fingerprint = true;
+        # Display key origins and updates
+        #with-key-origin = true;
+        # Cross-certify subkeys are present and valid
+        require-cross-certification = true;
+        # Disable caching of passphrase for symmetrical ops
+        no-symkey-cache = true;
+        # Enable smartcard
+        use-agent = true;
+        # Disable recipient key ID in messages
+        throw-keyids = true;
+        # Default/trusted key ID to use (helpful with throw-keyids)
+        default-key = "0x52ABD68B752E5959";
+        trusted-key = "0x52ABD68B752E5959";
+        # Group recipient keys (preferred ID last)
+        #"group keygroup" = "0xFF00000000000001 0xFF00000000000002 0xFF3E7D88647EBCDB";
+        # Keyserver URL
+        keyserver = [
+          "hkps://keys.openpgp.org"
+          "hkps://keyserver.ubuntu.com"
+          "hkps://hkps.pool.sks-keyservers.net"
+          "hkps://pgp.ocf.berkeley.edu"
+        ];
+        # Verbose output
+        #verbose = true;
+        # Show expired subkeys
+        #list-options = "show-unusable-subkeys";
+      };
     };
     htop = {
       enable = true;
@@ -1132,7 +1194,13 @@ in {
       client.enable = true;
     };
     flameshot.enable = true;
-    gpg-agent.enable = true;
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentryFlavor = "gnome3";
+      defaultCacheTtl = 60;
+      maxCacheTtl = 120;
+    };
     network-manager-applet.enable = true;
     nextcloud-client.enable = true;
     picom = {
