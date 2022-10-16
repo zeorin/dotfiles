3ce8e6b5cce866ad87fc9587d6a55c2a7cd8876c
Xandor Schiefer <me@xandor.co.za>
2022-10-16 19:50:30 +0200

    fix: newest wine doesn't work on NixOS
    Workaround for https://github.com/NixOS/nixpkgs/issues/189567

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2940,7 +2940,12 @@ in {
       httpie
       lxqt.lxqt-policykit
       xdg-user-dirs
-      wineWowPackages.stable
+      # https://github.com/NixOS/nixpkgs/issues/189567
+      # wineWowPackages.stable
+      (import (builtins.fetchTarball {
+        url =
+          "https://github.com/NixOS/nixpkgs/archive/7342cdc70156522050ce813386f6e159ca749d82.tar.gz";
+      }) { }).wineWowPackages.stable
       winetricks
       protontricks
       jrnl
