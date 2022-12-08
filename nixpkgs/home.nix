3a6a4872f4c58da6c5a3783a468ce15e941c99ed
Xandor Schiefer <me@xandor.co.za>
2022-12-08 13:15:17 +0200

    feat: docker support in DOOM Emacs

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2324,7 +2324,7 @@ in {
                  ;;ansible
                  ;;debugger          ; FIXME stepping through code, to help you add bugs
                  direnv
-                 ;;docker
+                 docker
                  editorconfig      ; let someone else argue about tabs vs spaces
                  ;;ein               ; tame Jupyter notebooks with emacs
                  (eval +overlay)     ; run code, run (also, repls)
