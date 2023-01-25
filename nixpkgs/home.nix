91d9308d98ce98df0ed33ba737b4baf791676dbb
Xandor Schiefer <me@xandor.co.za>
2023-01-25 14:00:08 +0200

    feat: `nix-index-update` script

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2953,6 +2953,14 @@ in {
       protontricks
       protonup
       jrnl
+      (writeShellScriptBin "nix-index-update" ''
+        # https://github.com/Mic92/nix-index-database#ad-hoc-download
+        filename="index-$(uname -m)-$(uname | tr A-Z a-z)"
+        mkdir -p ~/.cache/nix-index && cd ~/.cache/nix-index
+        # -N will only download a new version if there is an update.
+        wget -q -N https://github.com/Mic92/nix-index-database/releases/latest/download/$filename
+        ln -f $filename files
+      '')
       capitaine-cursors
       la-capitaine-icon-theme
       nordic
