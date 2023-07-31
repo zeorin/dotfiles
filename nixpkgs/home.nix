a8f9b5ebee60a4a89d7d4ff102939c376076fbe3
Xandor Schiefer <me@xandor.co.za>
2023-07-31 21:57:23 +0200

    chore: Change org-roam dailies directory

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -2764,9 +2764,12 @@ in {
 
         ;; If you use `org' and don't want your org files in the default location below,
         ;; change `org-directory'. It must be set before org loads!
-        (setq org-directory "~/Documents/notes"
-              org-roam-directory "~/Documents/notes"
-              org-agenda-files '("~/Documents/notes" "~/Documents/notes/daily"))
+        (setq org-directory "~/Documents/notes/"
+              org-roam-directory org-directory
+              org-roam-dailies-directory "journal/"
+              org-agenda-files (list org-roam-directory
+                                     (concat org-roam-directory
+                                             org-roam-dailies-directory)))
 
         ;; This determines the style of line numbers in effect. If set to `nil', line
         ;; numbers are disabled. For relative line numbers, set this to `relative'.
