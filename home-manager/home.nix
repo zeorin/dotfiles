f9779df1e2784733765a99a5eaa0537b2f1380a2
Xandor Schiefer <me@xandor.co.za>
2023-10-14 12:49:00 +0200

    fix: Open in Emacs using scheme handlers for
    - `file://`
    - `editor://`
    - `emacs://`

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -521,14 +521,17 @@ in {
       in rec {
         inherit EDITOR VISUAL;
         # Non-standard env var, found in https://github.com/facebook/react/pull/22649
-        EDITOR_URL = "editor://open?file={path}&line={line}";
+        EDITOR_URL = "editor://{path}:{line}";
         # Non-standard env var, found in https://github.com/yyx990803/launch-editor
         LAUNCH_EDITOR = pkgs.writeShellScript "launch-editor.sh" ''
-          filename=$1
-          line=$2
-          column=$3
+          file="$1"
+          line="$2"
+          column="$3"
 
-          ${my-emacs}/bin/emacsclient +$line:$column "$filename"
+          command="${pkgs.xdg-utils}/bin/xdg-open \"editor://$file\""
+          [ -n "$line" ] && command="$command:$line"
+          [ -n "$column" ] && command="$command:$column"
+          eval $command
         '';
         SUDO_EDITOR = VISUAL;
         LESS = "-FiRx4";
@@ -4690,41 +4693,118 @@ in {
       };
     };
     desktopEntries = {
-      editor-protocol = {
-        name = "editor protocol";
+      file-scheme-handler = {
+        name = "file:// scheme handler";
+        comment = "Open file in editor";
         exec = "${
-            (pkgs.stdenv.mkDerivation rec {
-              pname = "open-editor.sh";
-              version = "2.10.2";
-              src = pkgs.fetchFromGitHub {
-                owner = "nette";
-                repo = "tracy";
-                rev = "v${version}";
-                sha256 = "sha256-4bif5m7q0R8TGD6M0kYu8Dfx0uoq6RFW+UE9OQRr6a0=";
-                sparseCheckout =
-                  [ "tools/open-in-editor/linux/open-editor.sh" ];
-              };
-              dontConfigure = true;
-              dontBuild = true;
-              patchPhase = ''
-                substituteInPlace tools/open-in-editor/linux/open-editor.sh \
-                  --replace "#editor='emacs" "editor='${my-emacs}/bin/emacsclient"
-
-                chmod +x tools/open-in-editor/linux/open-editor.sh
-              '';
-              installPhase = ''
-                bindir="$out/bin"
-                install -d "$bindir"
-                install tools/open-in-editor/linux/open-editor.sh "$bindir"
-              '';
-              enableParallelBuilding = true;
-            })
-          }/bin/open-editor.sh %u";
-        icon = "emacs";
+            pkgs.writeShellScript "file-scheme-handler.sh" ''
+              uri="$1"
+              ${pkgs.xdg-utils}/bin/xdg-open "''${1//file:\/\//editor://}"
+            ''
+          } %u";
+        type = "Application";
+        terminal = false;
+        categories = [ "System" ];
+        mimeType = [ "x-scheme-handler/file" ];
+        noDisplay = true;
+      };
+      editor-scheme-handler = {
+        name = "editor:// scheme handler";
+        comment = "Open file in editor";
+        exec = "${
+            pkgs.writeShellScript "editor-scheme-handler.sh" ''
+              ${pkgs.xdg-utils}/bin/xdg-open "''${1//editor:\/\//emacs://}"
+            ''
+          } %u";
         type = "Application";
         terminal = false;
         categories = [ "System" ];
         mimeType = [ "x-scheme-handler/editor" ];
+        noDisplay = true;
+      };
+      emacs-scheme-handler = {
+        name = "emacs:// scheme handler";
+        comment = "Open file in Emacs";
+        exec = "${
+            pkgs.writeShellScript "emacs-scheme-handler.sh" ''
+              #!/usr/bin/env bash
+
+              # Unofficial Bash strict mode
+              set -euo pipefail
+
+              die() {
+                echo "$@" >&2
+                exit 1
+              }
+
+              declare file
+              declare line
+              declare column
+
+              # emacs://open?file={path}&line={line}&column={column}
+              # emacs://open/?file={path}&line={line}&column={column}
+              parseLikeJetbrains() {
+                readarray -t parsed < <(echo "$1" |
+                  awk \
+                    'match($0, /^emacs:\/\/(open|create|fix)\/?\?file=([^&]+)(&line=([0-9]+)(&column=([0-9]+))?)?$/, a) {
+                    print a[2]
+                    print a[4]
+                    print a[6]
+                  }')
+                file="''${parsed[0]-}"
+                line="''${parsed[1]-}"
+                column="''${parsed[2]-}"
+              }
+
+              # emacs://file/{path}:{line}:{column}
+              parseLikeVSCode() {
+                readarray -t parsed < <(echo "$1" |
+                  awk \
+                    'match($0, /^emacs:\/\/file(\/[^:]+)(:([0-9]+)(:([0-9]+))?)?$/, a) {
+                    print a[1]
+                    print a[3]
+                    print a[5]
+                  }')
+                file="''${parsed[0]-}"
+                line="''${parsed[1]-}"
+                column="''${parsed[2]-}"
+              }
+
+              # emacs:///{path}:{line}:{column}
+              # emacs://{host}/{path}:{line}:{column}
+              parseLikeFileURL() {
+                readarray -t parsed < <(echo "$1" |
+                  awk \
+                    'match($0, /^emacs:\/\/([^:]+)(:([0-9]+)(:([0-9]+))?)?$/, a) {
+                    print a[1]
+                    print a[3]
+                    print a[5]
+                  }')
+                file="''${parsed[0]-}"
+                line="''${parsed[1]-}"
+                column="''${parsed[2]-}"
+              }
+
+              parseLikeJetbrains "$1"
+              [ -z "$file" ] && parseLikeVSCode "$1"
+              [ -z "$file" ] && parseLikeFileURL "$1"
+              [ -z "$file" ] && die "Could not parse URI"
+
+              command="emacsclient --no-wait"
+              command="$command --eval '(find-file \"$file\")'"
+              [ -n "$line" ] && command="$command --eval '(goto-line $line)'"
+              [ -n "$column" ] && command="$command --eval '(move-to-column $column)'"
+              command="$command --eval '(recenter-top-bottom)'"
+              command="$command --eval '(select-frame-set-input-focus (selected-frame))'"
+              command="$command --eval '(when (functionp '\"'\"'pulse-momentary-highlight-one-line) (let ((pulse-delay 0.05)) (pulse-momentary-highlight-one-line (point) '\"'\"'highlight)))'"
+              eval $command
+            ''
+          } %u";
+        icon = "emacs";
+        type = "Application";
+        terminal = false;
+        categories = [ "System" ];
+        mimeType = [ "x-scheme-handler/emacs" ];
         noDisplay = true;
       };
       org-protocol = {
