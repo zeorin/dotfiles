8fa45a31dbfdf2be3745e194655c90c25bcf2ed5
Xandor Schiefer <me@xandor.co.za>
2024-03-24 14:25:11 +0200

    [WIP] feat: Blink video hardware acceleration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5516,10 +5516,33 @@ in {
               })
             ];
           }))
-      ]) ++ [
-        ungoogled-chromium
-        google-chrome
-        netflix
+      ]) ++ (let
+        mkChrome = (chrome:
+          (chrome.overrideAttrs (oldAttrs: {
+            postInstall = ''
+              wrapProgram $exe \
+                --set-default LIBVA_DRI3_DISABLE 1
+            '';
+          })).override {
+            commandLineArgs = ''
+              --disable-features=UseChromeOSDirectVideoDecoder \
+              --enable-features=TouchpadOverscrollHistoryNavigation
+            '';
+          });
+      in [ (mkChrome ungoogled-chromium) (mkChrome google-chrome) ]) ++ [
+        ((netflix.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = oldAttrs.nativeBuildInputs or [ ]
+            ++ [ makeWrapper ];
+          postInstall = ''
+            wrapProgram $out/bin/${netflix.name} \
+              --set-default LIBVA_DRI3_DISABLE 1
+          '';
+        })).override {
+          commandLineArgs = [
+            "--disable-features=UseChromeOSDirectVideoDecoder"
+            "--enable-features=TouchpadOverscrollHistoryNavigation"
+          ];
+        })
         tor-browser-bundle-bin
         virt-manager
         qemu
