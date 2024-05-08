95f909e9889f2b07d76137c60f040f6a4daf1f56
Xandor Schiefer <me@xandor.co.za>
2024-05-08 11:51:20 +0200

    [WIP] feat: mail

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -379,6 +379,8 @@ in
     { programs.nix-index-database.comma.enable = true; }
     inputs.nixos-vscode-server.homeModules.default
     { services.vscode-server.enable = true; }
+    ./email.nix
+    # ./email.nix
   ];
 
   options = with lib; {
