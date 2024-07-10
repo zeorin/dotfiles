e95905738458784df604a08537271de273b60fd6
Xandor Schiefer <me@xandor.co.za>
2024-07-10 10:02:59 +0200

    fix: don't use childframe for vertico

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -4423,7 +4423,7 @@ in {
                    ;;helm              ; the *other* search engine for love and life
                    ;;ido               ; the other *other* search engine...
                    ;;(ivy +childframe +icons +prescient)               ; a search engine for love and life
-                   (vertico +childframe +icons)           ; the search engine of the future
+                   (vertico +icons)           ; the search engine of the future
 
                    :ui
                    ;;deft              ; notational velocity for Emacs
