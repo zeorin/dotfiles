1cc2a417b4d2daa6d30b6b7a5ee66956c8a1eecb
Xandor Schiefer <me@xandor.co.za>
2024-12-30 11:35:27 +0200

    fix: Mount HIP lib path for software that has it hard-coded

--- nixos/guru/configuration.nix
+++ nixos/guru/configuration.nix
@@ -170,6 +170,25 @@
     amdvlk.support32Bit.enable = true;
   };
 
+  environment.systemPackages = with pkgs; [ lact ];
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+
+  systemd.tmpfiles.rules =
+    let
+      rocmEnv = pkgs.symlinkJoin {
+        name = "rocm-combined";
+        paths = with pkgs.rocmPackages; [
+          rocblas
+          hipblas
+          clr
+        ];
+      };
+    in
+    [
+      "L+    /opt/rocm   -    -    -     -    ${rocmEnv}"
+    ];
+
   hardware.keyboard.qmk.enable = true;
 
   # i2c
