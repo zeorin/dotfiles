dac06760a7f916dce0e7b0fe8d237318cd4775be
Xandor Schiefer <me@xandor.co.za>
2025-10-15 15:21:28 +0200

    feat: `podman`

--- nixos/common/configuration.nix
+++ nixos/common/configuration.nix
@@ -481,6 +481,7 @@
         "wheel"
         "networkmanager"
         "docker"
+        "podman"
         "adbusers"
         "libvirtd"
         "video"
@@ -614,8 +615,15 @@
     hardware.graphics.enable = true;
     hardware.graphics.enable32Bit = true;
 
-    virtualisation.docker.enable = true;
-    virtualisation.libvirtd.enable = true;
+    virtualisation = {
+      containers.enable = true;
+      podman = {
+        enable = true;
+        dockerCompat = true;
+        defaultNetwork.settings.dns_enabled = true;
+      };
+      libvirtd.enable = true;
+    };
 
     # Enable CUPS to print documents.
     services.printing.enable = true;
