17e69a059350ce1dc358a00c557fb4fbcc2ffbc3
Xandor Schiefer <me@xandor.co.za>
2025-10-15 10:23:26 +0200

    chore: update flake inputs & packages
    Added `devenv` flake because it provides a more current `devenv` version
    via overlays.

---
+++ nixos/common/cachix/devenv.nix
@@ -0,0 +1,7 @@
+{
+  nix.settings = {
+    substituters = [ "https://devenv.cachix.org" ];
+    trusted-substituters = [ "https://devenv.cachix.org" ];
+    trusted-public-keys = [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
+  };
+}
